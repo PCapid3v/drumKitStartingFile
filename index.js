123d757982ccdddbd4ac172970895922ed6a79b6
Pierluigi <pierluigi.capirci89@gmail.com>
2024-04-04 13:03:47 +0100

    amend : moving switch case into a function

--- index.js
+++ index.js
@@ -7,43 +7,50 @@ for (let i = 0; i < drumElements.length; i++) {
 
 function handleclick() {
     const buttonContext = this.textContent
-    switch (buttonContext) {
+
+    makeSound(buttonContext)    
+ 
+}
+
+
+const makeSound = (key) => {
+    switch (key) {
       case "w":
         let tom1 = new Audio("./sounds/tom-1.mp3");
         tom1.play();
-            break;
-        
+        break;
+
       case "a":
         let tom2 = new Audio("./sounds/tom-2.mp3");
         tom2.play();
-            break;
-        
+        break;
+
       case "s":
         let tom3 = new Audio("./sounds/tom-3.mp3");
         tom3.play();
-            break;
-        
+        break;
+
       case "d":
         let tom4 = new Audio("./sounds/tom-4.mp3");
         tom4.play();
-            break;
-        
+        break;
+
       case "j":
         let snare = new Audio("./sounds/snare.mp3");
         snare.play();
-            break;
-        
+        break;
+
       case "k":
         let crash = new Audio("./sounds/crash.mp3");
         crash.play();
-            break;
-        
+        break;
+
       case "l":
         let kick = new Audio("./sounds/kick-bass.mp3");
         kick.play();
-            break;
-        default: console.log(buttonContext)
+        break;
+      default:
+        console.log(key);
     }
- 
 }
 
